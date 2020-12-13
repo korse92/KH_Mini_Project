eb2f66597e6d00158702df5868dd5c01fc45259e
korse92 <korse92@gmail.com>
2020-12-13 14:37:33 +0900

    메인화면 -> 로그인,회원가입 선택창 -> 로그인,회원가입창 연결 완료

--- KH_MiniProject_MovieTicketReservation/src/movie/ticket/reservation/model/vo/NonMember.java
+++ KH_MiniProject_MovieTicketReservation/src/movie/ticket/reservation/model/vo/NonMember.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 
 public class NonMember extends Person{
 	
-	private String phoneNum;
+	
 	private String password;
 	
 	public NonMember() {
@@ -12,25 +12,16 @@ public class NonMember extends Person{
 	}
 
 	public NonMember(Calendar birth, String phoneNum, String password) {
-		super(null, birth);
+		super(null, birth, phoneNum);
 		
-		this.phoneNum = phoneNum;
 		this.password = password;
 	
-		String[] pNumArr = phoneNum.split("-");	
+		String[] pNumArr = super.getPhoneNum().split("-");	
 		String lastPhoneNum = pNumArr[2];
 		String birthStr = ""+ birth.get(Calendar.YEAR) + (birth.get(Calendar.MONTH)+1) + birth.get(Calendar.DATE);
 		
 		super.setName(birthStr + lastPhoneNum);
 		
-	}
-
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-
-	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;
 	}
 
 	public String getPassword() {
@@ -46,7 +37,6 @@ public class NonMember extends Person{
 		final int prime = 31;
 		int result = super.hashCode();
 		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
 		return result;
 	}
 
@@ -64,19 +54,18 @@ public class NonMember extends Person{
 				return false;
 		} else if (!password.equals(other.password))
 			return false;
-		if (getName() == null) {
-			if (other.getName() != null)
-				return false;
-		} else if (!getName().equals(other.getName()))
-			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "NonMember [getName()=" + getName() + ", getBirth()=" + getBirth()+ 
-				", phoneNum= " +phoneNum + ", password= " + password  + "]";
+		return "NonMember [getName()=" + getName() + ", getBirth()=" + getBirth()
+				+ ", getPhoneNum()=" + getPhoneNum() + ", password=" + password + "]";
 	}
+	
+	
+
+
 	
 	
 }
